3c2b244ce17b4260e09c480eb3afab82606e3556
Tnc84 <tancaubogdan@live.com>
2025-10-14 19:12:39 +0300

    Enhance home component layout and styles: Add door outline, adjust animal icons grid, and refine navbar design with new login and registration buttons. Update SCSS for improved responsiveness and visual consistency.

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
+  imports: [RouterModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
